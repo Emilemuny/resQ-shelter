8f8b30e575d1fa614e65ac461afec4a78c45c382
emilemuny <emilemuny007@gmail.com>
2015-02-18 14:22:34 -0800

    adding adopt feature code progress

--- server/routes/animals/index.js
+++ server/routes/animals/index.js
@@ -2,21 +2,30 @@
 
 var active = require('../../views/helpers/active');
 var Animal = require('../../models/animal');
+var _ = require('lodash');
 module.exports= {
   handler: function(request, reply) {
+   var q = request.query;
+
+   q = _.pick(q, function(v) {
+     return v;
+   });
+
+   if(q.min){
+     q.age = {$gte : q.min};
+     delete q.min;
+   }
+
+   if(q.max){
+     q.age = q.age || {};
+     _.merge(q.age, {$lte : q.max});
+     delete q.max;
+   }
+
+   q.isAdopted = false;
 
 
-
-
-    Object.keys(request.query).forEach(function(prop) {
-      if(!request.query[prop]) {
-        delete request.query[prop];
-      }
-    });
-
-    request.query.isAdopted = false;
-    console.log('query', request.query);
-    Animal.find(request.query, function(err, animals){
+    Animal.find(q, function(err, animals){
       reply.view('templates/animals/index',{path:'/animals', active:active, animals:animals});
 
     });
