315c82c784f81f0b1b8e80f0a726e7b2c3e3eafb
emilemuny <emilemuny007@gmail.com>
2015-02-18 12:58:25 -0800

    edit/delete working on the animals

--- server/config/routes.js
+++ server/config/routes.js
@@ -6,10 +6,20 @@ module.exports = [
   {method: 'get', path: '/about', config: require('../routes/general/about')},
   {method: 'get', path: '/faq', config: require('../routes/general/faq')},
   {method: 'get', path: '/contact', config: require('../routes/general/contact')},
+
   {method: 'get', path: '/animals', config: require('../routes/animals/index')},
   {method: 'get', path: '/animals/new', config: require('../routes/animals/new')},
   {method: 'post', path: '/animals', config: require('../routes/animals/create')},
   {method: 'get', path: '/animals/{animalId}', config: require('../routes/animals/show')},
   {method: 'post', path: '/animals/{animalId}/delete', config: require('../routes/animals/destroy')},
-  {method: 'get', path: '/animals/{animalId}/edit', config: require('../routes/animals/edit')}
+  {method: 'get', path: '/animals/{animalId}/edit', config: require('../routes/animals/edit')},
+  {method: 'post', path: '/animals/{animalId}', config: require('../routes/animals/update')},
+
+  {method: 'get', path: '/clients', config: require('../routes/clients/index')},
+  {method: 'get', path: '/clients/new', config: require('../routes/clients/new')},
+  {method: 'post', path: '/clients', config: require('../routes/clients/create')},
+  {method: 'get', path: '/clients/{clientId}', config: require('../routes/clients/show')},
+  {method: 'post', path: '/clients/{clientId}/delete', config: require('../routes/clients/destroy')},
+  {method: 'get', path: '/clients/{clientId}/edit', config: require('../routes/clients/edit')},
+  {method: 'post', path: '/clients/{clientId}', config: require('../routes/clients/update')}
 ];
--- server/routes/animals/index.js
+++ server/routes/animals/index.js
@@ -4,7 +4,19 @@ var active = require('../../views/helpers/active');
 var Animal = require('../../models/animal');
 module.exports= {
   handler: function(request, reply) {
-    Animal.find({isAdopted:false}, function(err, animals){
+
+
+
+
+    Object.keys(request.query).forEach(function(prop) {
+      if(!request.query[prop]) {
+        delete request.query[prop];
+      }
+    });
+
+    request.query.isAdopted = false;
+    console.log('query', request.query);
+    Animal.find(request.query, function(err, animals){
       reply.view('templates/animals/index',{path:'/animals', active:active, animals:animals});
 
     });
